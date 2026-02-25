718c20381bfd084a411ed82d691e4fbdd271faf9
1DevDynamo <byorderofpeakyblinders0001@gmail.com>
2026-02-25 17:19:31 +0530

    Time: 16 ms (14.94%) | Memory: 21.6 MB (9.09%) - LeetSync

--- 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
+++ 1019-squares-of-a-sorted-array/squares-of-a-sorted-array.py
@@ -4,14 +4,25 @@ class Solution:
         pos = [i for i in nums if i >= 0]
         neg = [i for i in nums if i < 0]
 
+        m = len(pos)
+        n = len(neg)
+
+        # no negative numbers
+        if n == 0:
+            pos = [i**2 for i in pos]
+            return pos
+        
+        # if no positive numbers
+        if m == 0:
+            neg = [i**2 for i in neg]
+            neg = neg[::-1]
+            return neg
+
+
         pos = [i**2 for i in pos]
         neg = [i**2 for i in neg]
 
         neg = neg[::-1]
-
-        m = len(pos)
-        n = len(neg)
-
         i,j,sid = 0,0,0
         solu = [0] *(m+n)
 
