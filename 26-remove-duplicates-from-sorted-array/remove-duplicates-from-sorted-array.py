51a5ff37faf3aa7fd0478123869f3b9300b10cc0
1DevDynamo <byorderofpeakyblinders0001@gmail.com>
2026-06-13 01:42:53 +0530

    Time: 4 ms (23.21%) | Memory: 20.6 MB (42.76%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.py
@@ -1,21 +1,14 @@
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         
-        un = 1       # first element is unique
-
-        p1 = 0       # pointer which moves only on unique element to count them
-
-        p2 = 1       # second pointer movies till n to check uniqueness and because first elemet is already uniquw p2=1
-
-        while(p2 < len(nums)):
-
-            if nums[p2] == nums[p2-1]:
-                p2 +=1
-            
-            else:
-                nums[p1+1] = nums[p2]
-                un += 1
-                p1 += 1
-                p2 += 1
+        if not nums:
+            return 0
         
-        return un
+        l = 0
+
+        for r in range(0,len(nums)):
+            if nums[r] != nums[l]:
+                l += 1
+                nums[l] = nums[r]
+
+        return l + 1
